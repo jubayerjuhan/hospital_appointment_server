28572bec4025b68cab9d2c9133e1ef7d88dc7a2f
Jubayer Juhan <davidjuhan23@gmail.com>
2023-07-20 22:52:32 +0600

    Appointment CRUD routes added

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -27,3 +27,70 @@ export const getAppointment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const editAppointment = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const updatedAppointment = await Appointment.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true }
+    );
+    if (!updatedAppointment) {
+      return next(new ErrorHandler(404, "Appointment not found"));
+    }
+    res.status(200).json({
+      success: true,
+      appointment: updatedAppointment,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteAppointment = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedAppointment = await Appointment.findByIdAndDelete(id);
+    if (!deletedAppointment) {
+      return next(new ErrorHandler(404, "Appointment not found"));
+    }
+    res.status(200).json({
+      success: true,
+      message: "Appointment Deleted",
+      appointment: deletedAppointment,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllAppointments = async (req, res, next) => {
+  try {
+    const appointments = await Appointment.find().populate(
+      "doctor patient",
+      "name username"
+    );
+    res.status(200).json({
+      success: true,
+      appointments,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserAppointment = async (req, res, next) => {
+  const patientId = req.params.id;
+  try {
+    const appointments = await Appointment.find({
+      patient: patientId,
+    }).populate("doctor patient", "name username");
+    res.status(200).json({
+      success: true,
+      appointments,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/appointmentRouter.js
+++ src/routes/appointmentRouter.js
@@ -2,11 +2,19 @@ import express from "express";
 import {
   createAppointment,
   getAppointment,
+  editAppointment,
+  deleteAppointment,
+  getAllAppointments,
+  getUserAppointment,
 } from "../controllers/appointmentController.js";
 
 const router = express.Router();
 
 router.route("/create-appointment").post(createAppointment);
 router.route("/get-appointment/:id").get(getAppointment);
+router.route("/edit-appointment/:id").put(editAppointment);
+router.route("/delete-appointment/:id").delete(deleteAppointment);
+router.route("/get-all-appointments").get(getAllAppointments);
+router.route("/get-user-appointments/:id").get(getUserAppointment);
 
 export default router;
